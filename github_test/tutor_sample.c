b906541636aa919769e17e995d7f08f1b8124b78
suntall <71126eun@naver.com>
2024-05-03 04:22:23 +0900

    초기 커밋.

---
+++ github_test/tutor_sample.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int tutor_01() {
+	printf("2020111349 이은경");
+}
+int main() {
+
+	//함수 만들고 학번 이름 출력하게 만들기
+	tutor_01();
+	//tutee_01(); //김혜민
+	//tutee_02(); //유연서
+	//tutee_03(); //김지호
+	//tutee_04(); //송지윤
+}
