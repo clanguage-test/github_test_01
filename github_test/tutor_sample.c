75d71fbce020f2b2095cf05d2d6fb393f465aafc
suntall <71126eun@naver.com>
2024-05-03 05:08:58 +0900

    [00] 김슈니

--- github_test/tutor_sample.c
+++ github_test/tutor_sample.c
@@ -4,10 +4,15 @@ int tutor_01() {
 	printf("2020111349 이은경");
 }
 
+int tutee_00() {
+	printf("2024xxxxxx 김슈니");
+}
+
 int main() {
 
 	//함수 만들고 학번 이름 출력하게 만들기
 	tutor_01();
+	tutee_00(); //김슈니
 	//tutee_01(); //김혜민
 	//tutee_02(); //유연서
 	//tutee_03(); //김지호
